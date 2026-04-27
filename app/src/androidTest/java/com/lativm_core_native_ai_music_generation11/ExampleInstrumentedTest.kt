48587e0d4fb7de0f865dd9b2aac10903eb35a57a
LATIVM <bng0401974@gmail.com>
2026-04-27 17:09:38 +0200

    Restarting: Modular architecture clean start

---
+++ app/src/androidTest/java/com/lativm_core_native_ai_music_generation11/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.lativm_core_native_ai_music_generation11
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.lativm_core_native_ai_music_generation11", appContext.packageName)
+    }
+}
